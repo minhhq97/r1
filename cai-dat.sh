a54500cd68543a7674f746efac94bc1c78eea15a
minhhq97 <61731173+minhhq97@users.noreply.github.com>
2025-12-23 17:31:23 +0700

    Update cai-dat.sh

--- cai-dat.sh
+++ cai-dat.sh
@@ -3,8 +3,8 @@
 rm -f $HOME/*.sh
 echo "Clean up old files done."
 
-wget -O $HOME/cai-dat-ai-v3.sh "https://d.uguu.se/hoXosMuD.sh"
-wget -O $HOME/cai-dat-dlna-unisound.sh "https://n.uguu.se/gwltPmBD.sh"
+wget -O $HOME/cai-dat-ai-v3.sh "https://github.com/minhhq97/r1/blob/5c4c0273142be87bd09cee5f46f60966c6c6b3c3/cai-dat-ai-v3.sh"
+wget -O $HOME/cai-dat-dlna-unisound.sh "https://github.com/minhhq97/r1/blob/5c4c0273142be87bd09cee5f46f60966c6c6b3c3/cai-dat-dlna-unisound.sh"
 chmod +x $HOME/cai-dat-ai-v3.sh
 chmod +x $HOME/cai-dat-dlna-unisound.sh
 
